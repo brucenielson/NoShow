8539df2f23dcb7a22da87fb59d7f0f14fc56f6d2
Bruce Nielson <brucenielson@users.noreply.github.com>
2021-05-15 23:31:49 -0600

    Got first version working with simple Logistic Regression

--- NoShow.py
+++ NoShow.py
@@ -1,11 +1,58 @@
 import os
 import pandas as pd
-import sklearn as sk
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 def main():
-   create_train_test_files()
+    # create_train_test_files()
+    X, Y = load_data('train.csv')
+    x_train, y_train, x_validate, y_validate = split_train_validation(X,Y)
+    clf = train(x_train, y_train)
+    y_pred = clf.predict(x_train)
+    # returns statistics
+    print("")
+    print("Training Set:")
+    print_statistics(y_pred, y_train)
+
+    # Check validation set
+    y_valdiate_pred = clf.predict(x_validate)
+    print("")
+    print("Validation Set:")
+    print_statistics(y_valdiate_pred, y_validate)
+
+
+
+def print_statistics(y_pred, y_actuals):
+    print('Misclassified samples: %d out of %d' % ((y_actuals != y_pred).sum(), len(y_actuals)))
+    print('Accuracy of set: %.2f' % accuracy_score(y_actuals, y_pred))
+
+
+
+def train(x_train, y_train):
+    clf = LogisticRegression(penalty='l2', C=1.0, max_iter=1000)
+    clf.fit(x_train, y_train)
+    return clf
+
+
+def split_train_validation(X, Y):
+    # Always split validation set in a consistent way
+    size = len(X)
+    split_at = int(size * 0.8)
+    x_train = X[0:split_at]
+    x_validate = X[split_at:]
+    y_train = Y[0:split_at]
+    y_validate = Y[split_at:]
+    return x_train, y_train.to_numpy().flatten(), x_validate, y_validate.to_numpy().flatten()
+
+
+
+def load_data(file_name='train.csv'):
+    file_name = os.getcwd() + '\\train.csv'
+    df = pd.read_csv(file_name)
+    return df[df.columns[0:len(df.columns)-1]], df[[df.columns[-1]]]
+
 
 
 def label_encode(df, column_name):
@@ -27,11 +74,21 @@ def create_train_test_files():
     # encode columns with labeler (i.e. not one hot encoding)
     X = label_encode(X, 'Gender')
     # Calculate difference between schedule and appointment
-    X['ScheduledDay'] = pd.to_datetime(X['ScheduledDay'])
-    X['AppointmentDay'] = pd.to_datetime(X['AppointmentDay'])
-    X['DiffDates'] = (X['AppointmentDay'] - X['ScheduledDay'])
+    X['ScheduledDay'] = pd.to_datetime(X['ScheduledDay']).dt.date
+    X['AppointmentDay'] = pd.to_datetime(X['AppointmentDay']).dt.date
+    X['DiffDates'] = (X['AppointmentDay'] - X['ScheduledDay']).dt.days
     # Drop unneeded columns
     X = X.drop(columns=['ScheduledDay', 'AppointmentDay'])
+    # Do one hot encoding on Neighbourhood
+    X = pd.get_dummies(X, columns=['Neighbourhood'])
+
+    # Scale and center
+    col_names = ['Age', 'DiffDates']
+    features = X[col_names]
+    scaler = preprocessing.StandardScaler().fit(features.values)
+    scaled = scaler.transform(features.values)
+    X[col_names] = scaled
+
     # Encode result field
     Y = df[['No-show']]
     Y = label_encode(Y, 'No-show')
@@ -56,6 +113,7 @@ def create_train_test_files():
     assert len(y_train) == len(train2)
     assert len(x_test) == len(test2)
     assert len(y_test) == len(test2)
+
 
 
 main()
